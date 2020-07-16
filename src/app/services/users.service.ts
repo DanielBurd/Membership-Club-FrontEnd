34147af2987820b19671511358e67930825dd15f
DanielBurd <danielb18@gmail.com>
2020-07-16 18:51:45 +0300

    to deploy

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -9,27 +9,27 @@ export class UsersService {
   constructor(private myHttpClient:HttpClient){ }
 
   public getAllUsersAsync():Promise<UsersModel[]>{
-    const observable=this.myHttpClient.get<UsersModel[]>('http://localhost:3000/users');
+    const observable=this.myHttpClient.get<UsersModel[]>('https://tapuch-api.herokuapp.com/users');
     return observable.toPromise();
   }
 
   public getUserByPhoneAsync(phone:string):Promise<UsersModel>{
-      const observable=this.myHttpClient.get<UsersModel>('http://localhost:3000/users/'+phone);
+      const observable=this.myHttpClient.get<UsersModel>('https://tapuch-api.herokuapp.com/users/'+phone);
       return observable.toPromise();
   }
 
   public createUserAsync(user:UsersModel):Promise<UsersModel>{
-      const observable=this.myHttpClient.post<UsersModel>('http://localhost:3000/users',user);
+      const observable=this.myHttpClient.post<UsersModel>('https://tapuch-api.herokuapp.com/users',user);
       return observable.toPromise()
   }
 
   public updateUserAsync(phone:string, user:UsersModel):Promise<UsersModel>{
-      const observable=this.myHttpClient.patch<UsersModel>('http://localhost:3000/users/'+phone,user);
+      const observable=this.myHttpClient.patch<UsersModel>('https://tapuch-api.herokuapp.com/users/'+phone,user);
       return observable.toPromise();
   }
 
   public deleteUserAsync(phone:string):Promise<UsersModel>{
-      const observable=this.myHttpClient.delete<UsersModel>('http://localhost:3000/users/'+phone);
+      const observable=this.myHttpClient.delete<UsersModel>('https://tapuch-api.herokuapp.com/users/'+phone);
       return observable.toPromise();
   }
 }
